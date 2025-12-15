9dc8b879abbf135e828915ff748f0867cbf6d9e1
gubnik <danger8217.nik@gmail.com>
2025-12-16 01:30:08 +0300

    Implement basic Tailwind CSS

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 ARG PYTHON_VERSION=3.11
+
 FROM python:${PYTHON_VERSION}-slim AS builder
 
 ENV PYTHONDONTWRITEBYTECODE=1
@@ -12,6 +13,16 @@ RUN apt-get update && apt-get install -y --no-install-recommends build-essential
     && pip install --no-cache-dir -r requirements.txt \
     && apt-get remove -y build-essential gcc && apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
 
+RUN apt-get update && apt-get install -y --no-install-recommends curl \
+    && curl -fsSL https://deb.nodesource.com/setup_16.x | bash - \
+    && apt-get install -y nodejs \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
+
+COPY package.json tailwind.config.js postcss.config.js /app/
+COPY static/ /app/static/
+
+RUN npm install && npm run build
+
 FROM python:${PYTHON_VERSION}-slim AS runtime
 
 ENV PORT=6969
@@ -22,7 +33,9 @@ ARG UID=10001
 RUN adduser --disabled-password --gecos "" --uid ${UID} appuser
 
 WORKDIR /app
+
 COPY --from=builder /usr/local/ /usr/local/
+COPY --from=builder /app /app
 COPY . /app
 
 RUN chown -R appuser:appuser /app
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./**/*.html"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    0:  '#1F1F1F',
+                    1:  '#0F0F0F',
+                    2:  '#DF0D0F',
+                    3:  '#AF0D0B',
+                    4:  '#6DA06D',
+                    5:  '#5F7F1E',
+                    6:  '#DDDF0A',
+                    7:  '#AF0D0B',
+                    8:  '#DFBF0E',
+                    9:  '#2E6FDD',
+                    10: '#2E4AB4',
+                    11: '#AF6FDD',
+                    12: '#AF4FDD',
+                    13: '#2EDFBD',
+                    14: '#1BCAAA',
+                    15: '#FFFFFF',
+                    16: '#FFFFAF',
+                    17: '#3F3F3F',
+                    18: '#2F3F3F',
+                },
+                back: {
+                    0: '#2F1F1F',
+                    1: '#1F2F1F',
+                    2: '#1F1F2F',
+                    3: '#1F3F2F',
+                    4: '#2F5F4F',
+                }
+            }
+        },
+    },
+  plugins: [],
+}
+
